d4529b823cb17246839d014e42fe72689fbca831
dagatsoin <danil_fransman@hotmail.com>
2019-02-26 10:20:50 +0100

    [TS] update background image doc

--- demo/stories/backgroundImage/doc.ts
+++ demo/stories/backgroundImage/doc.ts
@@ -14,7 +14,7 @@ This component displays a background image inside a container. Does not accept c
   })}
 >
   <BackgroundImage
-    source={ uri: "https://i.ibb.co/Yhs3Ff5/btn-bg.png"}
+    uri="https://i.ibb.co/Yhs3Ff5/btn-bg.png"
     resizeMode="cover"
   />
   {children}
@@ -22,9 +22,9 @@ This component displays a background image inside a container. Does not accept c
 \`\`\`
 ### Properties
 #### source
-\`type: string | { uri: string }\`
+\`type: string\`
 
-The source of the image. Either a static image or an object containing the uri of the image to fetch.
+The uri of the image to fetch.
 
 #### resizeMode
 \`type: 'stretch' | 'contain' | 'cover' | 'auto'\`
@@ -38,6 +38,6 @@ If true, repeat the pattern along X and Y axis.
 ### position *optional*
 \`type: [string, string]\`
 
-Available in \`auto\` and \`stretch\` resize mode. A tupple of string which will offset the top/left of the image.
+Available in \`auto\` resize mode. A tupple of string which will offset the top/left of the image.
 eg: \`['50px', '50px'] or['50%', '50%']\`
 `
