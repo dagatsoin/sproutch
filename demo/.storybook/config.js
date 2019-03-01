9daf6f7ef1b572a12fde74a57a91a5754748ccc1
dagatsoin <danil_fransman@hotmail.com>
2019-03-01 09:00:56 +0100

    [FIX] Import Roboto font in demo

--- demo/.storybook/config.js
+++ demo/.storybook/config.js
@@ -4,16 +4,23 @@ import { withNotes } from '@storybook/addon-notes'
 
 import { defaultTheme, ThemeContext } from '@sproutch/ui'
 import fontAwesome from 'react-native-vector-icons/Fonts/FontAwesome.ttf'
+import robotoRegular from '../assets/fonts/roboto/Roboto-Regular.ttf'
 
 const fontAwesomeStyle = `@font-face {
   src: url(${fontAwesome});
   font-family: FontAwesome;
 }`
+const robotoRegularStyle = `@font-face {
+  src: url(${robotoRegular});
+  font-family: Roboto;
+}`
 
 // Create stylesheet
 const faStyleEl = document.createElement('style');
+const robotoRegularStyleEl = document.createElement('style');
 
 faStyleEl.type = 'text/css';
+robotoRegularStyleEl.type = 'text/css';
 
 if (faStyleEl.styleSheet) {
   faStyleEl.styleSheet.cssText = fontAwesomeStyle;
@@ -21,8 +28,15 @@ if (faStyleEl.styleSheet) {
   faStyleEl.appendChild(document.createTextNode(fontAwesomeStyle));
 }
 
+if (robotoRegularStyleEl.styleSheet) {
+  robotoRegularStyleEl.styleSheet.cssText = robotoRegularStyle;
+} else {
+  robotoRegularStyleEl.appendChild(document.createTextNode(robotoRegularStyle));
+}
+
 // Inject stylesheet
 document.head.appendChild(faStyleEl);
+document.head.appendChild(robotoRegularStyleEl);
 
 // Center the story in the screen
 const Center = (storyFn) => (
