e67a07287ecbf65314374e105eeb7171c562ea5c
dagatsoin <danil_fransman@hotmail.com>
2019-03-22 17:31:50 +0100

    [TS] export PaperStyle and PaperStyleOverride

--- src/components/paper/index.native.ts
+++ src/components/paper/index.native.ts
@@ -1,1 +1,3 @@
 export { default as Paper } from './Paper.native'
+export { PaperProps } from './PaperProps'
+export { PaperStyle, PaperStyleOverride } from './PaperStyle'
--- src/components/paper/index.ts
+++ src/components/paper/index.ts
@@ -1,2 +1,3 @@
 export { default as Paper } from './Paper.web'
 export { PaperProps } from './PaperProps'
+export { PaperStyle, PaperStyleOverride } from './PaperStyle'
