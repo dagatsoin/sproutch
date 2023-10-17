7250e42e22d63b7f25c10d759d48cfb5ff796119
Daniel Neveux <d.neveux@warfog.gg>
2023-10-17 11:08:08 +0200

    chore: fix metro config

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-
-import { Preview } from '@storybook/react';
-
-import { ThemeProvider, defaultTheme } from '@sproutch/ui'
-
-import { FontProvider } from '../FontDecorator'
-import { Story } from '@storybook/react-native';
-
 export const parameters = {
   controls: {
     matchers: {
@@ -15,17 +6,3 @@ export const parameters = {
     },
   }
 }
-
-const preview: Preview = {
-  decorators: [
-    (Story: Story) => (
-      <FontProvider>
-        <ThemeProvider theme={defaultTheme}>
-          <Story />
-        </ThemeProvider>
-      </FontProvider>
-    ),
-  ],
-};
-
-export default preview;
--- packages/storybook/metro.config.js
+++ packages/storybook/metro.config.js
@@ -1,21 +1,24 @@
-import { getDefaultConfig } from 'expo/metro-config';
-import path from 'path';
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
+// The current project where metro is called from
 const projectRoot = __dirname;
+
+// The root of the mono repo where the node_modules are
 const monorepoRoot = path.resolve(projectRoot, '../..');
 
-const config = getDefaultConfig(monorepoRoot);
+const config = getDefaultConfig(projectRoot);
 
 // Only list the packages within your monorepo that your app uses. No need to add anything else.
 // If your monorepo tooling can give you the list of monorepo workspaces linked
 // in your app workspace, you can automate this list instead of hardcoding them.
 const monorepoPackages = {
-  '@sproutch/ui': path.resolve(monorepoRoot, 'packages/components'),
+  '@sproutch/ui': path.resolve(monorepoRoot, 'packages/components/src'),
 };
 
 // 1. Watch the local app folder, and only the shared packages (limiting the scope and speeding it up)
 // Note how we change this from `workspaceRoot` to `projectRoot`. This is part of the optimization!
-config.watchFolders = [projectRoot, ...Object.values(monorepoPackages)];
+config.watchFolders = [monorepoRoot];
 
 if(config.resolver) {
   // Add the monorepo workspaces as `extraNodeModules` to Metro.
@@ -30,6 +33,22 @@ if(config.resolver) {
     path.resolve(monorepoRoot, 'node_modules'),
   ];
 
+  // Load fonts
+ /*  config.resolver.resolveRequest = (context, moduleName, platform) => {
+    if (moduleName.startsWith('./vendor/react-native-vector-icons/Fonts/FontAwesome5_Brands.ttf')) {
+      // Logic to resolve the module name to a file path...
+      // NOTE: Throw an error if there is no resolution.
+      return {
+        filePaths: [path.resolve(monorepoRoot, 'node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Brands.ttf')],
+        type: "assetFiles",
+      };
+    }
+    // Optionally, chain to the standard Metro resolver.
+    return context.resolveRequest(context, moduleName, platform);
+  } */
+
   // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
   config.resolver.disableHierarchicalLookup = true;
 }
+
+module.exports = config;
